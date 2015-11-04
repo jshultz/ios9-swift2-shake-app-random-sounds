9d4143645f706b07278de5be01c8175f32a0eb0e
Jason Shultz <jasshultz@gmail.com>
2015-11-03 18:53:26 -0700

    added more code

--- shake-that-app/ViewController.swift
+++ shake-that-app/ViewController.swift
@@ -7,12 +7,66 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player:AVAudioPlayer = AVAudioPlayer() // Create a player
+    var tune:String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        
+        swipeRight.direction = UISwipeGestureRecognizerDirection.Right
+        
+        self.view.addGestureRecognizer(swipeRight)
+        
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: "swiped:")
+        
+        swipeUp.direction = UISwipeGestureRecognizerDirection.Up
+        
+        self.view.addGestureRecognizer(swipeUp)
+        
+        
+    }
+    
+    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
+        if event?.subtype == UIEventSubtype.MotionShake {
+            print("device was shaken, not stirred")
+        }
+    }
+    
+    func playSound(tune:String) {
+        
+        let audioPath = NSBundle.mainBundle().pathForResource(tune, ofType: "mp3")! // path to audio file
+        
+        do {
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: audioPath))
+        } catch {
+            print("siomething went wrong")
+        }
+        
+    }
+    
+    func swiped(gesture:UIGestureRecognizer){
+        if let swipegesture = gesture as? UISwipeGestureRecognizer {
+            
+            switch swipegesture.direction {
+            case UISwipeGestureRecognizerDirection.Right:
+                
+                print("swipe right")
+            case UISwipeGestureRecognizerDirection.Up:
+                let audioPath = NSBundle.mainBundle().pathForResource("mozart-k311-3-sasaki", ofType: "mp3")! // path to audio file
+                print("swipe up")
+            default:
+                break
+            }
+            
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
